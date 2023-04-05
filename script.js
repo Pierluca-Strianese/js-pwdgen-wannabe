5caa3bba11f9477c4a277e2f8d54c9817449d8db
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-05 17:47:57 +0200

    COMPLETE

---
+++ script.js
@@ -0,0 +1,13 @@
+const nome = prompt('Quale e\' il tuo nome?');
+
+const cognome = prompt('Quale e\' il tuo cognome?');
+
+const colore = prompt('Quale e\' il tuo colore preferito?');
+
+const parolafinale = nome + cognome + colore;
+
+
+console.log('nome:', nome);
+console.log('cognome:', cognome);
+console.log('colore:', colore);
+console.log('parola:', parolafinale);
